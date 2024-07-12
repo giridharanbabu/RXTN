e7f47db34d27a4b055b023918d7948b2f90f3d0f
giridharababu <giri1208srinivas@gmail.com>
2024-07-12 22:51:54 +0530

    changes single file to multiple file upload in chat messages

--- pkg/database/database.py
+++ pkg/database/database.py
@@ -1,3 +1,5 @@
+import gridfs
+from bson import ObjectId
 from pymongo import MongoClient
 
 
@@ -5,6 +7,7 @@ class Database:
     def __init__(self, mongo_uri, db_name):
         self.client = MongoClient(mongo_uri)
         self.db = self.client[db_name]
+        self.fs = gridfs.GridFS(self.db)  # Initialize GridFS
 
     def get_collection(self, collection_name):
         return self.db[collection_name]
@@ -19,12 +22,30 @@ class Database:
     def collection_exists(self, collection_name):
         return collection_name in self.db.list_collection_names()
 
+    def store_file(self, file_data, filename):
+        return self.fs.put(file_data, filename=filename)
 
+    def get_file(self, file_id):
+        try:
+            file = self.fs.get(ObjectId(file_id))
+            return file
+        except gridfs.NoFile:
+            return None
+
+    def get_file_metadata(self, file_id):
+        return self.fs.get(file_id)
+
+    def delete_file(self, file_id):
+        self.fs.delete(file_id)
+
+
+# Example usage
 MONGO_URI = ("mongodb+srv://giri1208srinivas:mongouser@cluster0.extptud.mongodb.net/?retryWrites=true&w=majority"
              "&appName=Cluster0")
 DB_NAME = 'rxtn'
 
 database = Database(MONGO_URI, DB_NAME)
+
 user_collection = database.get_collection('users')
 
 print(database)
